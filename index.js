c823211bef61d4eb5f63fb32b783ba0b901edce4
Dariusz Jagieło <dariusz.jagielo@gmail.com>
2018-07-24 22:16:12 +0200

    small refactor, structure for testing

--- index.js
+++ index.js
@@ -19,23 +19,29 @@ module.exports.getBatteryPercentageInfo = function (interval) {
 }
 
 function readBatteryPercentage() {
-    return from(exec('ioreg -r -l -k "BatteryPercent"')).pipe(map( result => {
-        return result.stdout.split('+-o').slice(1).
-            map(el => {
-                return el.replace(new RegExp('"', 'g'), '').replace(new RegExp(' ', 'g'), '').split('\n').filter(line => {
-                    return line.startsWith('Product=') || line.startsWith('BatteryPercent=')
-                });
-            }).map(device => {
-                let deviceName = '';
-                let batteryPercent = 0;
-                device.forEach(property => {
-                    if (property.startsWith('Product=')) {
-                        deviceName = property.replace(new RegExp('Product=', 'g'), '');
-                    } else if (property.startsWith('BatteryPercent=')) {
-                        batteryPercent = property.replace(new RegExp('BatteryPercent=', 'g'), '');
-                    }
-                });
-                return new BatteryPercentage(deviceName, batteryPercent);
+    return from(exec('ioreg -r -l -k "BatteryPercent"')).
+        pipe(
+            map(mapStdOut)
+        );
+}
+
+function mapStdOut(result) {
+    const res = result.stdout.split('+-o').slice(1).
+        map(el => {
+            return el.replace(new RegExp('"', 'g'), '').replace(new RegExp(' ', 'g'), '').split('\n').filter(line => {
+                return line.startsWith('Product=') || line.startsWith('BatteryPercent=')
             });
-    }));
+        }).map(device => {
+            let deviceName = '';
+            let batteryPercent = 0;
+            device.forEach(property => {
+                if (property.startsWith('Product=')) {
+                    deviceName = property.replace(new RegExp('Product=', 'g'), '');
+                } else if (property.startsWith('BatteryPercent=')) {
+                    batteryPercent = property.replace(new RegExp('BatteryPercent=', 'g'), '');
+                }
+            });
+            return new BatteryPercentage(deviceName, batteryPercent);
+        });
+    return res;
 }
---
+++ test/test.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const expect = require('chai').expect;
+const magic_battery = require('../index');
